d44ee1a8acf27d5a8ddc42e0a6163802169529f1
cubert3d <cubert3d@gmail.com>
2021-07-09 14:02:16 -0500

    0.1.4.1: made entity control not render saddles on all horses

--- src/main/java/me/cubert3d/palladium/event/mixin/mixins/HorseBaseEntityMixin.java
+++ src/main/java/me/cubert3d/palladium/event/mixin/mixins/HorseBaseEntityMixin.java
@@ -6,6 +6,7 @@ import net.minecraft.util.ActionResult;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.Redirect;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
 @Mixin(HorseBaseEntity.class)
@@ -19,11 +20,18 @@ abstract class HorseBaseEntityMixin {
         }
     }
 
-    @Inject(method = "isSaddled()Z", at = @At("HEAD"), cancellable = true)
-    private void isSaddledInject(CallbackInfoReturnable<Boolean> info) {
+    /*
+    Only target the isSaddled call in travel so that the entity-control module does
+    not render saddles on all horses, regardless of whether they actually have saddles.
+     */
+    @Redirect(method = "travel", at = @At(value = "INVOKE", target = "Lnet/minecraft/entity/passive/HorseBaseEntity;isSaddled()Z"))
+    private boolean isSaddledRedirect(HorseBaseEntity horse) {
         ActionResult result = EntityControlCallback.EVENT.invoker().interact();
         if (result.equals(ActionResult.SUCCESS)) {
-            info.setReturnValue(true);
+            return true;
+        }
+        else {
+            return horse.isSaddled();
         }
     }
 }
